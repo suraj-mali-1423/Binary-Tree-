d85a55b8a4c837b1c89e7214af493709c01d24db
suraj-mali-1423 <surajsmali@coep.sveri.ac.in>
2025-12-05 10:59:00 +0530

    543. Diameter of Binary Tree
    Given the root of a binary tree, return the length of the diameter of the tree.
    
    The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
    
    The length of a path between two nodes is represented by the number of edges between them.
    
    Input: root = [1,2,3,4,5]
    Output: 3
    Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
    Example 2:
    
    Input: root = [1,2]
    Output: 1

---
+++ Tree_Question/DiameterOfBinaryTre.java
@@ -0,0 +1,34 @@
+package Tree_Question;
+import binaryTree.BinaryTree;
+import binaryTree.Node;
+
+import static Tree_Question.HeightOfTree.heightOfTree;
+
+public class DiameterOfBinaryTre {
+    public static  int Diameter(Node root){
+            if(root == null || (root.left == null && root.right == null)) return 0;
+            int leftAns = Diameter(root.left);
+            int rightAns = Diameter(root.right);
+
+            int mid = heightOfTree(root.left) + heightOfTree(root.right);
+            if(root.left != null) mid++;
+            if(root.right != null) mid++;
+
+            int max = Math.max(leftAns , Math.max(rightAns , mid));
+
+            return mid;
+    }
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.insertRoot(1);             // root = 1
+        tree.insertLeft(tree.root, 2);
+        tree.insertRight(tree.root, 3);
+        tree.insertLeft(tree.root.left, 4);
+        tree.insertRight(tree.root.left, 5);
+        tree.insertLeft(tree.root.right, 6);
+        tree.insertRight(tree.root.right, 7);
+
+        System.out.println(Diameter(tree.root));
+    }
+}
